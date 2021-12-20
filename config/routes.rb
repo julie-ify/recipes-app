5b3c3fbe3c408a9f0433e245776c4e954ff52587
Juliana Ifionu <julieifionu@gmail.com>
2021-12-20 13:25:48 +0100

    Add migration changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'inventory_foods/index'
+  get 'inventories/index'
+  get 'recipes/index'
+  get 'recipe_foods/index'
+  get 'foods/index'
+  get 'users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 end
