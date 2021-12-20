03b7eb9eb6d569c5797a91b7ccd5932dfbfebc90
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-20 16:06:31 +0200

    add abilities model

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can :read, Recipe, public: true
+    can :manage, Recipe, user: user
+    can :manage, Food, user: user
+    can :manage, Inventory, user: user
+    can :read, Food
+    can :create, :all
+  end
+end
