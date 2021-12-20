ae4001c42c43cb2cfef8fa7368ded7297a1b2b63
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-20 20:03:23 +0200

    update routes, css and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'inventory_foods/index'
-  get 'inventories/index'
-  get 'recipes/index'
-  get 'recipe_foods/index'
-  get 'foods/index'
-  get 'users/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'foods#index'
+  resources :foods, only: [:index, :new, :create, :destroy]
 end
